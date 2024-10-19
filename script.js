7f7f76eb24be2e9403155ef073e5bf82817d19eb
sunsetsinger <sunsetsinger@gmail.com>
2024-10-18 22:10:17 -0400

    Co-authored-by: Justin Morneau <MaXianbibi@users.noreply.github.com>

--- script.js
+++ script.js
@@ -2,17 +2,12 @@ const bg_video = document.getElementById("background-video");
 const header_1 = document.getElementById("header-1");
 const header_2 = document.getElementById("header-2");
 
-
-
 function toggle(background, color1, color2, word) {
-  bg_video.src = background;
-
-  
-  header_1.style.background = `linear-gradient(135deg, ${color1 }, ${color2})`;
-  header_2.style.background = `linear-gradient(135deg, ${color1 }, ${color2})`;
-  header_2.innerHTML = word;
-  
+    bg_video.src = background;
+   
+    header_1.style.background = `linear-gradient(135deg, ${color1}, ${color2})`;
+    header_2.style.background = `linear-gradient(135deg, ${color1}, ${color2})`;
+    header_2.textContent = word;
 }
-
 
 
