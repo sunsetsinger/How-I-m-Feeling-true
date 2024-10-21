4abc741389a317d86061026e700ec02b47fde7b3
sunsetsinger <sunsetsinger@gmail.com>
2024-10-21 15:37:11 -0400

    background ease

--- script.js
+++ script.js
@@ -3,7 +3,9 @@ const header_1 = document.getElementById("header-1");
 const header_2 = document.getElementById("header-2");
 
 function toggle(background, color1, color2, word) {
+    bg_video.style.transition = "ease 500ms";
     bg_video.src = background;
+    
    
     header_1.style.background = `linear-gradient(135deg, ${color1}, ${color2})`;
     header_2.style.background = `linear-gradient(135deg, ${color1}, ${color2})`;
